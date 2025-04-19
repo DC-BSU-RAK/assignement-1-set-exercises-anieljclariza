2c98db3840ddd444e84d97d89fcda9aa95bd92e4
Aniel-J Clariza <ajdcz@proton.me>
2025-04-19 11:36:43 +0400

    Changes

--- Chapter-5-Embedded Content/Exercises/Audio Sampler/index.js
+++ Chapter-5-Embedded Content/Exercises/Audio Sampler/index.js
@@ -1,9 +1,15 @@
+// Select all <button> and store in buttons variable
 const buttons = document.querySelectorAll("button");
 
+// Loop through each button
 buttons.forEach((button) => {
+  // Listen for a click event
   button.addEventListener("click", () => {
+    // Grab value from button-sound
     const fileName = button.getAttribute("button-sound");
+    // Make new Audio object and point to file in audio directory
     const audio = new Audio(`audio/${fileName}.mp3`);
+    // Play audio
     audio.play();
   });
 });
---
+++ Chapter-6-Javascript/Exercises/RGB Color Guessing Game/index.js
@@ -0,0 +1,10 @@
+const rgbDisp = document.getElementById("rgbDisplay");
+const options = document.getElementById("colorOptions");
+const info = document.getElementById("gameInfo");
+const livesDisp = document.getElementById("lives");
+const scoreDisp = document.getElementById("score");
+const restart = document.getElementById("restart");
+
+let correctColor;
+let lives = 3;
+let score = 0;
